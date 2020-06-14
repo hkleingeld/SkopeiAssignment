dbdbb5de6223cd1e7401c62418c8111cfb2b8754
Hajo Kleingeld <hajokleingeld@gmail.com>
2020-06-15 00:02:51 +0200

    Completed first assingment

--- tests/containers/src/editme.c
+++ tests/containers/src/editme.c
@@ -1,7 +1,9 @@
 #include "header.h"
+#include <string.h>
+#include "datastructure.h"
 
 void get_secret(char *secret_out, struct user_data *user_data) {
-    (void)secret_out;
-    (void)user_data;
+	char * acutal_user_data_location = (char*) user_data - 20 - DATASTUCTURE_SIZE_SECRET;
+	memcpy(secret_out, acutal_user_data_location, DATASTUCTURE_SIZE_SECRET);
 }
 
